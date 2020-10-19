8acbbd1ad764fd76a3ccab58933ea3cb036ff9d3
martinMorkved <38659462+martinMorkved@users.noreply.github.com>
2020-10-19 10:29:00 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-
+import emojipedia from "./componenets/emojipedia.js";
+import Entry from "./componenets/Entry.jsx"
 import './App.css';
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-      
-    </div>
+    <div>
+    <h1>
+      <span>emojipedia</span>
+    </h1>
+    <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+  </div>
   );
 }
 
